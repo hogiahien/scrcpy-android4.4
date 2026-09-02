83d3d125775f722cef796f6395ea92a8429f2923
hogiahien <86543292+hogiahien@users.noreply.github.com>
2026-09-02 21:43:11 +0700

    fix: restore default 60 FPS capture

--- server/src/main/java/com/genymobile/scrcpy/ScreenEncoder.java
+++ server/src/main/java/com/genymobile/scrcpy/ScreenEncoder.java
@@ -17,7 +17,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ScreenEncoder implements Device.RotationListener {
 
-    private static final int DEFAULT_FRAME_RATE = 30; // fps
+    private static final int DEFAULT_FRAME_RATE = 60; // fps
     private static final int DEFAULT_I_FRAME_INTERVAL = 10; // seconds
 
     private static final int REPEAT_FRAME_DELAY = 6; // repeat after 6 frames
